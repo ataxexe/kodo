891ee1172a8cbdd57907675a3a7b3c4fa220c3cb
Ataxexe <ataxexe@gmail.com>
2014-06-02 13:33:46 -0300

    improvements

--- src/main/java/org/kodo/Should.java
+++ src/main/java/org/kodo/Should.java
@@ -26,6 +26,8 @@
 
 package org.kodo;
 
+import org.hamcrest.Matcher;
+
 import java.util.Collection;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -33,6 +35,9 @@ import java.util.function.Predicate;
 import static org.junit.Assert.*;
 
 /**
+ * Helper class that contains usefull methods to create the assertions for the
+ * fluent interface.
+ *
  * @author Marcelo Guimarães
  */
 public class Should {
@@ -41,58 +46,124 @@ public class Should {
 
   }
 
+  /**
+   * Indicates that the value should be <code>true</code>
+   */
   public static Consumer<Boolean> BE_TRUE = beTrue();
 
+  /**
+   * Indicates that the value should be <code>false</code>
+   */
   public static Consumer<Boolean> BE_FALSE = beFalse();
 
+  /**
+   * Indicates that the value should be <code>null</code>
+   */
   public static Consumer BE_NULL = beNull();
 
+  /**
+   * Indicates that the value should not be <code>null</code>
+   */
   public static Consumer NOT_BE_NULL = notBeNull();
 
+  /**
+   * Indicates that the value should be a collection and
+   * {@link java.util.Collection#isEmpty() empty}
+   */
   public static Consumer<Collection> BE_EMPTY = beEmpty();
 
+  /**
+   * Indicates that the value should not be a collection and
+   * {@link java.util.Collection#isEmpty() empty}
+   */
   public static Consumer<Collection> NOT_BE_EMPTY = notBeEmpty();
 
+  /**
+   * Indicates that the value should be <code>true</code>
+   */
   public static Consumer<Boolean> beTrue() {
     return be(true);
   }
 
+  /**
+   * Indicates that the value should be <code>false</code>
+   */
   public static Consumer<Boolean> beFalse() {
     return be(false);
   }
 
+  /**
+   * Indicates that the value should be <code>null</code>
+   */
   public static Consumer beNull() {
     return (obj) -> assertNull(obj);
   }
 
+  /**
+   * Indicates that the value should not be <code>null</code>
+   */
   public static Consumer notBeNull() {
     return (obj) -> assertNotNull(obj);
   }
 
+  /**
+   * Indicates that the value should
+   * {@link java.lang.Object#equals(Object) equal} the given value.
+   */
   public static Consumer be(Object value) {
     return (obj) -> assertEquals(value, obj);
   }
 
+  /**
+   * Indicates that the value should not
+   * {@link java.lang.Object#equals(Object) equal} the given value.
+   */
   public static Consumer notBe(Object value) {
     return (obj) -> assertNotEquals(value, obj);
   }
 
+  /**
+   * Indicates that the value should {@link Predicate#test(Object) match} the
+   * given predicate.
+   */
   public static Consumer be(Predicate predicate) {
     return (obj) -> assertTrue(predicate.test(obj));
   }
 
+  /**
+   * Indicates that the value should not {@link Predicate#test(Object) match}
+   * the given predicate.
+   */
   public static Consumer notBe(Predicate predicate) {
     return (obj) -> assertFalse(predicate.test(obj));
   }
 
+  /**
+   * Indicates that the value should be a collection and
+   * {@link java.util.Collection#isEmpty() empty}
+   */
   public static Consumer<Collection> beEmpty() {
     return (collection) -> assertTrue(collection.isEmpty());
   }
 
+  /**
+   * Indicates that the value should not be a collection and
+   * {@link java.util.Collection#isEmpty() empty}
+   */
   public static Consumer<Collection> notBeEmpty() {
     return (collection) -> assertFalse(collection.isEmpty());
   }
 
+  /**
+   * Indicates that the value should match the given matcher.
+   */
+  public static Consumer match(Matcher matcher) {
+    return obj -> assertThat(obj, matcher);
+  }
+
+  /**
+   * Indicates that the operation should throw the given exception.
+   */
   public static Consumer raise(Class<? extends Throwable> exception) {
     return (error) -> {
       if (error != null) {
@@ -103,6 +174,9 @@ public class Should {
     };
   }
 
+  /**
+   * Indicates that the operation should not throw any exceptions.
+   */
   public static Consumer succeed() {
     return (error) -> {
       if (error != null) {
