d4b35b8db72ee65941298fa58447c00bcfea7e50
Ataxexe <ataxexe@devnull.tools>
2017-02-01 21:45:42 -0200

    remove empty predicate

--- src/main/java/tools/devnull/kodo/Predicates.java
+++ src/main/java/tools/devnull/kodo/Predicates.java
@@ -8,43 +8,6 @@ import java.util.function.Predicate;
 public interface Predicates {
 
   /**
-   * Indicates that the value should be <code>true</code>
-   */
-  Predicate<Boolean> TRUE = value -> value;
-
-  /**
-   * Indicates that the value should be <code>false</code>
-   */
-  Predicate<Boolean> FALSE = value -> !value;
-
-  /**
-   * Indicates that the value should be <code>null</code>
-   */
-  Predicate NULL = obj -> obj == null;
-
-  /**
-   * Indicates that the value should not be <code>null</code>
-   */
-  Predicate NOT_NULL = obj -> obj != null;
-
-  /**
-   * Indicates a value that should be empty
-   *
-   * @see EmptyPredicate
-   */
-  Predicate EMPTY = new EmptyPredicate();
-
-  /**
-   * Indicates a value that should be empty
-   *
-   * @return a predicate that checks if the value is empty
-   * @see EmptyPredicate
-   */
-  static <T> Predicate<T> empty() {
-    return EMPTY;
-  }
-
-  /**
    * Predicate that returns <code>true</code> if the value is greather than
    * the given one
    */
--- src/test/java/tools/devnull/kodo/PredicatesTest.java
+++ src/test/java/tools/devnull/kodo/PredicatesTest.java
@@ -4,10 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static tools.devnull.kodo.Predicates.FALSE;
-import static tools.devnull.kodo.Predicates.NOT_NULL;
-import static tools.devnull.kodo.Predicates.NULL;
-import static tools.devnull.kodo.Predicates.TRUE;
 import static tools.devnull.kodo.Predicates.greaterThan;
 import static tools.devnull.kodo.Predicates.greaterThanOrEqual;
 import static tools.devnull.kodo.Predicates.lessThan;
@@ -17,33 +13,6 @@ import static tools.devnull.kodo.Predicates.lessThanOrEqual;
  * The test suite for the {@link Predicates}
  */
 public class PredicatesTest {
-
-  @Test
-  public void testNull() {
-    assertTrue(NULL.test(null));
-    assertFalse(NULL.test(""));
-
-    assertTrue(NOT_NULL.test(""));
-    assertFalse(NOT_NULL.test(null));
-  }
-
-  @Test
-  public void testBeTrue() {
-    assertTrue(TRUE.test(true));
-    assertFalse(TRUE.test(false));
-  }
-
-  @Test
-  public void testBeFalse() {
-    assertTrue(FALSE.test(false));
-    assertFalse(FALSE.test(true));
-  }
-
-  @Test
-  public void testBeNull() {
-    assertTrue(NULL.test(null));
-    assertFalse(NULL.test("string"));
-  }
 
   @Test
   public void testGreaterThan() {
