3a4c7f00a4cd36d8e5dff08afc2a15da1751750f
Marcelo Guimarães <ataxexe@backpackcloud.com>
2019-06-13 00:05:30 +0300

    prepare to update the groupId

--- src/main/java/tools/devnull/kodo/Expectation.java
+++ src/main/java/tools/devnull/kodo/Expectation.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2014 Marcelo "Ataxexe" Guimarães <ataxexe@devnull.tools>
+ * Copyright (c) 2014 Marcelo Guimaraes <ataxexe@backpackcloud.com>
  *
  * Permission  is hereby granted, free of charge, to any person obtaining
  * a  copy  of  this  software  and  associated  documentation files (the
